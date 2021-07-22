224a1e7a2e4bd6b3fd35cd8bff22b0313b6dead8
ivancostilla <ivan20punto88@gmail.com>
2021-07-22 16:02:42 -0300

    configuracion de firestore - 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyBWnt3k1PnGU0YFbXs4l9iaQjSTbDoA_WY",
+    authDomain: "control-clientes-15ba0.firebaseapp.com",
+    projectId: "control-clientes-15ba0",
+    storageBucket: "control-clientes-15ba0.appspot.com",
+    messagingSenderId: "913722144129",
+    appId: "1:913722144129:web:42f81f20e212ebdee9e480",
+    measurementId: "G-3LNMG0Z2TZ"
+  },
 };
