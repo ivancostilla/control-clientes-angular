f36c2ac4041c36943ebe37fffe724444588f4bd1
ivancostilla <ivan20punto88@gmail.com>
2021-07-23 17:49:07 -0300

    editar y eliminar cliente

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -40,6 +40,29 @@ export class ClienteServicio{
 
   agregarCliente(cliente: Cliente){
     this.clientesColeccion.add(cliente)
+  };
+  getCliente(id: string){
+    this.clienteDoc = this.db.doc<Cliente>(`clientes/${id}`);
+    this.cliente = this.clienteDoc.snapshotChanges().pipe(
+      map( accion => {
+        if(accion.payload.exists === false){
+          return null;
+        } else {
+          const datos = accion.payload.data() as Cliente;
+          datos.id = accion.payload.id;
+          return datos as any;
+        }
+      })
+    );
+      return this.cliente;
+  };
+  modificarCliente(cliente: Cliente){
+    this.clienteDoc = this.db.doc(`clientes/${cliente.id}`);
+    this.clienteDoc.update(cliente);
+  };
+  eliminarCliente(cliente: Cliente){
+    this.clienteDoc = this.db.doc(`clientes/${cliente.id}`);
+    this.clienteDoc.delete();
   }
 
 }
