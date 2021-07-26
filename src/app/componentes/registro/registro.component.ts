2a2275ed7965b38e9ac24ccad981ca5c9865e26b
ivancostilla <ivan20punto88@gmail.com>
2021-07-26 17:22:42 -0300

    componente de registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { LoginService } from 'src/app/servicios/login.service';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private flashMessagesService: FlashMessagesService,
+    private loginService: LoginService,
+  ) { }
 
   ngOnInit(): void {
+    //esto es para que una vez estemos logueados la pagina
+    //de login ya no se muestre
+    this.loginService.getAuth().subscribe(auth => {
+      if(auth){
+        this.router.navigate(['/'])
+      }
+    })
+  };
+
+  registro(){
+    this.loginService.registrarse(this.email,this.password)
+    .then( res => {
+      this.router.navigate(['/'])
+    })
+    .catch(
+      error => {
+        this.flashMessagesService.show(error.message,{
+          cssClass: 'alert-danger', timeout: 4000
+        })
+      }
+    )
   }
 
 }
--- src/app/servicios/login.service.ts
+++ src/app/servicios/login.service.ts
@@ -28,6 +28,17 @@ export class LoginService{
 
   logout(){
     this.authService.signOut();
+  };
+
+  registrarse(email:string,password:string){
+    return new Promise((resolve,reject)=>{
+      this.authService.createUserWithEmailAndPassword(email,password)
+      .then(
+        datos => resolve(datos),
+        error => reject(error),
+      )
+    })
+
   }
 
 }
